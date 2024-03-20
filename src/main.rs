a8181da00b91806d4fb72f51d15ea4e124829b0d
Andri <andri@bizoftenterprise.com>
2024-03-20 17:23:20 +0700

    pass 3nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::Write, net::TcpListener};
+use std::str;
+use std::{io::Read, io::Write, net::TcpListener};
+
+const MAX_BUFFER_SIZE: usize = 2048;
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -9,7 +12,24 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let _ = stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
+                let mut buf = [0; MAX_BUFFER_SIZE];
+                let len = stream.peek(&mut buf).expect("peek failed");
+                let buf = &buf[..len - 1];
+                let string = String::from_utf8(buf.to_vec()).expect("failed convert string");
+                let parts = string.split("\r\n").collect::<Vec<&str>>();
+                let part = parts[0].split(" ").collect::<Vec<&str>>()[1];
+                match part {
+                    "/" => {
+                        let _ = stream
+                            .write(b"HTTP/1.1 200 OK\r\nHALO\r\n\r\n")
+                            .expect("Error write stream");
+                    }
+                    _ => {
+                        let _ = stream
+                            .write(b"HTTP/1.1 400 Not Found\r\nHALO\r\n\r\n")
+                            .expect("Error write stream");
+                    }
+                };
             }
             Err(e) => {
                 println!("error: {}", e);
